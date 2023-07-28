f99a2bd9f1174eb5b59bdeae4f80863f75c6d9ba
Rajesh <itsyaboiinitial1ze@gmail.com>
2023-07-29 02:51:46 +0530

    finish slider

--- script.js
+++ script.js
@@ -27,3 +27,44 @@ function toggleClassBlogCategory(clickedBlogItem) {
 
     activeBlogItem = clickedBlogItem;
 };
+
+
+
+let idx = 8;
+
+function resizeImageContainer() {
+    const imageSliderContainer = document.querySelector('.image-slider-container');
+    const divs = imageSliderContainer.querySelectorAll('div');
+    const individualWidth = 189.9;
+
+    const divWidth = divs.length * individualWidth;
+    const width = document.body.clientWidth;
+
+    const extraSpace = width - divWidth;
+    const extraDivs = Math.abs(Math.ceil(extraSpace / individualWidth));
+
+    for (let i = 0; i < extraDivs; i++) {
+        idx = (idx % 8 + 8) % 8;
+        const clonedDiv = divs[idx].cloneNode(true);
+
+        if (extraSpace > 0) {
+            imageSliderContainer.appendChild(clonedDiv);
+            idx++;
+        }
+        else {
+            imageSliderContainer.removeChild(imageSliderContainer.lastChild);
+            idx--;
+        }
+    }
+
+    const currScreenWidth = document.body.clientWidth;
+    const numberOfDivs = imageSliderContainer.children.length;
+    const newIndividualWidth = currScreenWidth / numberOfDivs;
+
+    imageSliderContainer.querySelectorAll('div').forEach(div => div.style.width = `${newIndividualWidth}px`);
+}
+
+window.addEventListener('load', resizeImageContainer);
+
+window.addEventListener('resize', resizeImageContainer);
+
